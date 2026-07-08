8bf42788f8f3b121847c835df4e8322e5e1cb250
Saw <synwill7777@gmail.com>
2026-07-08 11:56:03 -0700

    Use root base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/saw_Portfolio/',
   server: {
     port: 3000,
   },
